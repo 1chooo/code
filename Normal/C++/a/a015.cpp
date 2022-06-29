bf1a0cfec8a2427ba2e22f814023bd84c1f63bc4
1chooo <hugo970217@gmail.com>
2022-06-30 01:26:55 +0800

    a

--- Normal/C++/a/a015.cpp
+++ Normal/C++/a/a015.cpp
@@ -1,6 +1,6 @@
 /*
 *  Problem link: https://zerojudge.tw/ShowProblem?problemid=a015
-*  Status:
+*  Status: AC
 */
 
 #include <iostream>
@@ -9,6 +9,39 @@ using namespace std;
 
 int main () 
 {
+    int row, column;
+    int matrix[100][100];
+    int matrixT[100][100];
+
+    while (cin >> row >> column)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                cin >> matrix[i][j];
+            }
+        }
+
     
+
+        for (int i = 0; i < row + column; i++)
+        {
+            for (int j = 0; j < row + column; j++)
+            {
+                matrixT[j][i] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < column; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                cout << matrixT[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
     return 0;
 }
