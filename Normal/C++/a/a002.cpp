b2d6ae95843d66106b0227883a7460026220ba59
1chooo <hugo970217@gmail.com>
2022-06-29 16:50:04 +0800

    a

---
+++ Normal/C++/a/a002.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string inputString;
+
+    cin >> inputString;
+
+    cout << "hello, " << inputString << endl;
+
+    return 0;
+}
