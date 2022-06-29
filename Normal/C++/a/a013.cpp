e7d58d04f9a2074ef9e950b02e893eb82c44628c
1chooo <hugo970217@gmail.com>
2022-06-30 01:00:23 +0800

    a

---
+++ Normal/C++/a/a013.cpp
@@ -0,0 +1,15 @@
+/*
+*  Problem link: https://zerojudge.tw/ShowProblem?problemid=a013
+*  Status:
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+    
+    return 0;
+}
---
+++ Normal/C++/a/a015.cpp
@@ -0,0 +1,14 @@
+/*
+*  Problem link: https://zerojudge.tw/ShowProblem?problemid=a015
+*  Status:
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+    
+    return 0;
+}
