d24ce1d39a077d8569f02123b785df74ceec02d0
1chooo <hugo970217@gmail.com>
2022-06-29 23:35:04 +0800

    a

---
+++ Normal/C++/a/a009.cpp
@@ -0,0 +1,24 @@
+/*
+*  https://zerojudge.tw/ShowProblem?problemid=a009
+*  Status: AC
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string x;
+    cin >> x;
+
+    for (int i = 0; i < x.length(); i++)
+    {
+      char n = x[i];
+      n = n - 7;
+      cout << n;
+    }
+
+    return 0;
+}
