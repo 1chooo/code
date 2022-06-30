5f87f84e132157e3cbc20840fc59ddb65905d4cc
1chooo <hugo970217@gmail.com>
2022-07-01 02:01:07 +0800

    a

---
+++ Normal/C++/a/a017.cpp
@@ -0,0 +1,21 @@
+/*
+*  Problem link: https://zerojudge.tw/ShowProblem?problemid=a017
+*  Status: 
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    string inputString;
+
+    while (cin >> inputString)
+    {
+         string temp[INT16_MAX];
+         
+    }
+    return 0;
+}
