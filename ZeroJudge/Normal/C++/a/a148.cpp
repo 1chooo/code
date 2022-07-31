af3aa6ca67a83332e9beb9ebe2de368a49deca70
1chooo <hugo970217@gmail.com>
2022-08-01 00:56:26 +0800

    ZeroJudge

---
+++ ZeroJudge/Normal/C++/a/a148.cpp
@@ -0,0 +1,21 @@
+/*
+ Problem link: https://zerojudge.tw/ShowProblem?problemid=a148
+ Status: 
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void) {
+    int n;
+
+    while (cin >> n) {
+        for (int i = 0; i < n; i++) {
+            
+        }
+    }
+
+    return 0;
+}
