ef75300b144c6454452cd81f988cdd38201b79ea
1chooo <hugo970217@gmail.com>
2022-08-13 01:25:58 +0800

    ZeroJudge

---
+++ ZeroJudge/Normal/e/e926.cpp
@@ -0,0 +1,18 @@
+/*
+ Problem link: https://zerojudge.tw/ShowProblem?problemid=e926
+ Status: AC
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void) {
+    cout << "|\'o\'|" << endl;
+    cout << "\\^_^/" << endl;
+    cout << "(\"o\")" << endl;
+    cout << "[-%-]" << endl;
+
+    return 0;
+}
