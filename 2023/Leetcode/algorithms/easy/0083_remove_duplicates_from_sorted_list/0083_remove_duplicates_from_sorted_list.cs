1f446ac003b54e32be9a5e23ea35dbccf6f0262e
1chooo <hugo970217@gmail.com>
2023-12-08 22:02:00 +0800

    add: 83. Remove Duplicates from Sorted List

---
+++ 2023/Leetcode/algorithms/easy/0083_remove_duplicates_from_sorted_list/0083_remove_duplicates_from_sorted_list.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
