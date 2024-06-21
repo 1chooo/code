91f268a09c7d033c1f85989bf091e32da8dbe4f2
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-06-22 01:44:00 +0800

    feat: uva 100 The 3n + 1 problem

---
+++ uva/100_the_3n+1_problem/test.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void) {
+
+  long long int i, j, temp, n;
+  long long int operation, maxOperation;
+
+  while (cin >> i >> j) {
+
+    bool change = false;
+
+    if (i > j) {
+      temp = i;
+      i = j;
+      j = temp;
+      change = true;
+    }
+
+    maxOperation = 0;
+
+    for (int a = i; a <= j; a++) {
+      n = i;        // here occur the error.
+      operation = 1;
+
+      while (1) {
+        if (n == 1)
+          break;
+        else if (n % 2 == 1)
+          n = 3 * n + 1;
+        else
+          n /= 2;
+        operation++;
+      }
+
+      if (operation > maxOperation)
+        maxOperation = operation;
+    }
+
+    if (change == true) 
+      cout << j << " " << i << " " << maxOperation << endl;
+    else 
+      cout << i << " " << j << " " << maxOperation << endl;
+  }
+  
+  return 0;
+}
