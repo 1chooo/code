8a4d669acdea1d05f13cc7ffe5306ad350fbaf72
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-02 08:21:50 +0800

    feat(zerojudge): a001 哈囉

---
+++ zerojudge/a001/a001.cpp
@@ -0,0 +1,19 @@
+/*
+*  Problem link: https://zerojudge.tw/ShowProblem?problemid=a001
+*  Status: AC
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string inputString;
+
+    cin >> inputString;
+    
+    cout << "hello, " << inputString << endl;
+
+    return 0;
+}
---
+++ zerojudge/a001/a001.py
@@ -0,0 +1,7 @@
+# Problem link: https://zerojudge.tw/ShowProblem?problemid=a001
+# Status: AC
+
+
+inputString = str(input())
+
+print("hello,", inputString)
