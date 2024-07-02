78b8513b0b53df5546bdc72d7e81ca36072e4a2f
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-03 00:30:36 +0800

    feat(zerojudge): a002 簡易加法

---
+++ zerojudge/a002/a002.cpp
@@ -0,0 +1,18 @@
+/*
+ *  Problem link: https://zerojudge.tw/ShowProblem?problemid=a002
+ *  Status: AC
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int first, second;
+
+    cin >> first >> second;
+
+    cout << (first + second) << endl;
+
+    return 0;
+}
