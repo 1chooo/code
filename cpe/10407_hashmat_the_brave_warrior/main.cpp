24bc15ea20ff6ec0a76c8ee08a661760c25d4e39
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-04 20:14:17 +0800

    feat(cpe): 10407 Hashmat the brave warrior

---
+++ cpe/10407_hashmat_the_brave_warrior/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Author: @1chooo
+ * Title: 10055 - Hashmat the brave warrior
+ * Required: Time limit: 3.000 seconds
+ * Link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=996
+ * Status: AC
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+
+  long long int army1, army2;
+
+  while(cin >> army1 >> army2)
+    cout << abs(army1 - army2) << endl;
+  
+  return 0;
+}
