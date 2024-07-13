c3c9a2f489357914015a874f57e8626763fdeb38
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-13 14:01:30 +0800

    feat(zerojudge): a022 迴文

---
+++ zerojudge/a022_迴文/a022.cpp
@@ -0,0 +1,34 @@
+/*
+*  Problem link: https://zerojudge.tw/ShowProblem?problemid=a022
+*  Status: AC
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    string inStr;
+    int inStrLen, pivot, count;
+
+    cin >> inStr;
+    inStrLen = inStr.length();
+
+    if (inStrLen % 2 == 0) {
+        pivot = inStrLen / 2;
+    } else {
+        pivot = (inStrLen - 1) / 2;
+    }
+
+    for (int i = 0; i < pivot; i++) {
+            if (inStr[i] == inStr[inStrLen - 1 - i])
+                count++;
+    }
+
+    if (count == pivot)
+        cout << "yes" << endl;
+    else
+        cout << "no" << endl;
+    
+    return 0;
+}
