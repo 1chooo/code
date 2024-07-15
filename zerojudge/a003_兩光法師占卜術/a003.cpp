5313c8702121113c98d075e8966b78bb98dc45c1
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-15 17:03:22 +0800

    feat(zerojudge): a003 兩光法師占卜術

---
+++ zerojudge/a003_兩光法師占卜術/a003.cpp
@@ -0,0 +1,34 @@
+/*
+*  Problem link: https://zerojudge.tw/ShowProblem?problemid=a003
+*  Status: AC
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int decideFortune(int, int);
+
+int main() 
+{
+    int month, day;
+
+    cin >> month >> day;
+    decideFortune(month, day);
+    return 0;
+}
+
+int decideFortune (int month, int day) {
+    int s = (month * 2 + day) % 3;
+
+    if (s == 0) {
+        cout << "普通" << endl;
+    } else if (s == 1) {
+        cout << "吉" << endl;
+    } else {
+        cout << "大吉" << endl;
+    }
+
+    return 0;
+}
