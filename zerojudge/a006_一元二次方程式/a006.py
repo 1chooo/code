014863065082b1923a370725f70d1bdc7daea095
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-17 22:46:28 +0800

    feat(zerojudge): a006 一元二次方程式

---
+++ zerojudge/a006_一元二次方程式/a006.py
@@ -0,0 +1,24 @@
+# Problem link: https://zerojudge.tw/ShowProblem?problemid=a006
+# Status: AC
+
+inputString = str(input()).split(" ")
+
+a = int(inputString[0])
+b = int(inputString[1])
+c = int(inputString[2])
+
+def discriminant(a, b, c) :
+    d = float(b * b - 4 * a * c)    # It can also be written as integer.
+
+    if (d > 0.0) :
+        x1 = int((-b + d ** (1 / 2)) / 2 * a)
+        x2 = int((-b - d ** (1 / 2)) / 2 * a)
+        print(f"Two different roots x1={x1} , x2={x2}")
+    elif (d == 0.0) :
+        x = int((-b) / (2 * a))
+        print(f"Two same roots x={x}")
+    else :
+        print("No real root")
+    return
+
+discriminant(a, b, c)
