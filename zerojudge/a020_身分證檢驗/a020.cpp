a069bc62aad943604f8c3e1ec0e724cbafa6c10f
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-19 17:49:51 +0800

    feat(zerojudge): a020 身分證檢驗

---
+++ zerojudge/a020_身分證檢驗/a020.cpp
@@ -0,0 +1,43 @@
+/*
+*  Problem link: https://zerojudge.tw/ShowProblem?problemid=a020
+*  Status: AC
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string city = "ABCDEFGHJKLMNPQRSTUVXYWZIO";
+    string id;
+    int enNum, total;
+    int num = 8;
+
+    cin >> id;
+
+    for (int i = 0; i < 26; i++) {
+        if (id[0] == city[i]) {
+            enNum = i + 10;
+        }
+    }
+
+    total = (enNum / 10) + (9 * (enNum % 10));
+
+    int temp = (int) id[9] - 48;
+    total += temp;
+    
+    for (int i = 1; i < 9; i++) {
+        int temp = ((int) id[i] - 48) * num;
+        total += temp;
+        num--;
+    }
+
+    if (total % 10 == 0)
+        cout << "real\n";
+    else
+        cout << "fake\n";
+
+    return 0;
+}
