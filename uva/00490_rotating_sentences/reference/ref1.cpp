56c50652e766bde7cd54b389d493954603d18bd2
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-23 00:24:43 +0800

    feat(uva): 00490 Rotating Sentences

--- uva/00490_rotating_sentences/reference/ref1.cpp
+++ uva/00490_rotating_sentences/reference/ref1.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
-int main()
-{
-  string a[101];
-  int n = 0;
-  int max = 0;
-  while (getline(cin, a[n]) && a[n] != "")
-  {
-    if (a[n].length() > max)
-    {
-      max = a[n].length();
+
+int main() {
+    string a[101];
+    int n = 0;
+    int max = 0;
+    while (getline(cin, a[n]) && a[n] != "") {
+        if (a[n].length() > max) {
+            max = a[n].length();
+        }
+        n++;
     }
-    n++;
-  }
-  for (int i = 0; i < max; i++)
-  {
-    for (int j = n - 1; j >= 0; j--)
-    {
-      if (a[j].length() > i)
-      {
-        cout << a[j][i];
-      }
-      else
-      {
-        cout << ' ';
-      }
+    for (int i = 0; i < max; i++) {
+        for (int j = n - 1; j >= 0; j--) {
+            if (a[j].length() > i) {
+                cout << a[j][i];
+            } else {
+                cout << ' ';
+            }
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 }
--- uva/00490_rotating_sentences/reference/ref2.cpp
+++ uva/00490_rotating_sentences/reference/ref2.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-int main()
-{
-  string s[101];
-  int index = 0;
-  int max = 0;
+int main() {
+    string s[101];
+    int index = 0;
+    int max = 0;
 
-  while (getline(cin, s[index]))
-  {
-    if (max < s[index].length())
-    { // find out the max length of strings
-      max = s[index].length();
+    while (getline(cin, s[index])) {
+        if (max < s[index].length()) { // find out the max length of strings
+            max = s[index].length();
+        }
+        index++;
     }
-    index++;
-  }
 
-  for (int i = 0; i < max; i++)
-  {
-    for (int j = index - 1; j >= 0; j--)
-    {
-      if (s[j].length() > i)
-      {
-        cout << s[j][i];
-      }
-      else
-      {
-        cout << ' ';
-      }
+    for (int i = 0; i < max; i++) {
+        for (int j = index - 1; j >= 0; j--) {
+            if (s[j].length() > i) {
+                cout << s[j][i];
+            } else {
+                cout << ' ';
+            }
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
-  return 0;
+    return 0;
 }
