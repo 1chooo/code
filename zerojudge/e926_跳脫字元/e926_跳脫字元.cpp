e136ae004d25d4ad89c353f93dadba098b8d5f6f
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-25 00:31:24 +0800

    feat(zerojudge): e926 跳脫字元

---
+++ zerojudge/e926_跳脫字元/e926_跳脫字元.cpp
@@ -0,0 +1,18 @@
+/*
+ Problem link: https://zerojudge.tw/ShowProblem?problemid=e926
+ Status: AC
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << "|\'o\'|" << endl;
+    cout << "\\^_^/" << endl;
+    cout << "(\"o\")" << endl;
+    cout << "[-%-]" << endl;
+
+    return 0;
+}
