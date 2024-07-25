37186a95458d237befb036234e05a75f246e43ce
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-26 00:11:37 +0800

    feat(zerojudge): a410 解方程

---
+++ zerojudge/a410_解方程/a410_解方程.cpp
@@ -0,0 +1,34 @@
+/*
+ Problem link: https://zerojudge.tw/ShowProblem?problemid=a410
+ Status: AC
+ Reference: https://zh.wikipedia.org/wiki/%E5%85%8B%E8%90%8A%E5%A7%86%E6%B3%95%E5%89%87
+*/
+
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    double a, b, c;
+    double d, e, f;
+
+    cin >> a >> b >> c;
+    cin >> d >> e >> f;
+
+    if (a * e == b * d) {
+        if (a * f == c * d)
+            cout << "Too many" << endl;
+        else
+            cout << "No answer" << endl;
+    } else {
+        double x = (c * e - b * f) / (a * e - b * d);
+        double y = (c * d - a * f) / (b * d - a * e);
+
+        printf("x=%.2f\n", x);
+        printf("y=%.2f\n", y);
+    }
+
+    return 0;
+}
