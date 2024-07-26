f05146b5c9331b7f6f7e33741eb7b73cd6cd5de4
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-27 01:33:46 +0800

    feat(zerojudge): a215 明明愛數數

---
+++ zerojudge/a215_明明愛數數/a215_明明愛數數.cpp
@@ -0,0 +1,30 @@
+/*
+ Problem link: https://zerojudge.tw/ShowProblem?problemid=a215
+ Status: AC
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void) {
+    int n, m;
+
+    while (cin >> n >> m) {
+        int count = 0;
+        int sum = 0;
+        for (int i = n; i <= (10000 + n); i++) {
+            count++;
+            sum += i;
+
+            if (sum > m) {
+                break;
+            }
+        }
+
+        cout << count << endl;
+    }
+
+    return 0;
+}
