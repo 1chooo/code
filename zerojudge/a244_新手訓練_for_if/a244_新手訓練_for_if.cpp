5c7222e40dca891ac90c30922325a8ee658e9c93
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-27 01:32:19 +0800

    feat(zerojudge): a244 新手訓練~for+if

---
+++ zerojudge/a244_新手訓練_for_if/a244_新手訓練_for_if.cpp
@@ -0,0 +1,41 @@
+/*
+ Problem link: https://zerojudge.tw/ShowProblem?problemid=a224
+ Status: AC
+*/
+
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int N, a;
+    long long int b, c;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> a >> b >> c;
+
+        switch (a) {
+        case 1:
+            cout << b + c;
+            break;
+        case 2:
+            cout << b - c;
+            break;
+        case 3:
+            cout << b * c;
+            break;
+        case 4:
+            cout << b / c;
+            break;
+        default:
+            break;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
