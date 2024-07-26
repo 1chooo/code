864fb2bd0a04a87c8e38cc254c941e2b527056ef
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-27 01:30:11 +0800

    feat(zerojudge): a741 Bangla Numbers

---
+++ zerojudge/a741_bangla_numbers/ref2.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void split(long long n) {
+    if (n >= 10000000) {
+        split(n / 10000000);
+        cout << " kuti";
+        n %= 10000000;
+    }
+    if (n >= 100000) {
+        split(n / 100000);
+        cout << " lakh";
+        n %= 100000;
+    }
+    if (n >= 1000) {
+        split(n / 1000);
+        cout << " hajar";
+        n %= 1000;
+    }
+    if (n >= 100) {
+        split(n / 100);
+        cout << " shata";
+        n %= 100;
+    }
+    if (n)
+        cout << " " << n;
+}
+
+int main() {
+    long long n;
+    int kase = 1;
+    while (cin >> n) {
+        cout << setw(4) << kase++ << ".";
+        if (n)
+            split(n);
+        else
+            cout << " 0";
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ zerojudge/a741_bangla_numbers/ref3.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+#include <algorithm>
+
+using namespace std;
+
+int k[] = {10000000, 100000, 1000, 100};
+
+string s[] = {"kuti", "lakh", "hajar", "shata"}, answer;
+
+string str(long long int x)
+
+{
+    string s = "";
+
+    while (x)
+
+    {
+        s += '0' + (x % 10);
+
+        x /= 10;
+    }
+
+    reverse(s.begin(), s.end());
+
+    return s;
+}
+
+void fix(long long int x)
+
+{
+    if (x >= k[0])
+
+    {
+        fix(x / k[0]);
+
+        answer += " " + s[0];
+
+        x %= (int)k[0];
+    }
+
+    for (int i = 0; i < 4; i++)
+
+    {
+        if (x / k[i] > 0)
+
+        {
+            answer += " " + str(x / k[i]) + " " + s[i];
+
+            x %= k[i];
+        }
+    }
+
+    if (x > 0)
+        answer += " " + str(x);
+}
+
+int main()
+
+{
+    ios_base::sync_with_stdio(0);
+
+    cin.tie(0);
+
+    long long int N;
+
+    int Case = 1;
+
+    while (cin >> N)
+
+    {
+        cout << "   " << Case++ << ".";
+
+        if (N == 0)
+            cout << " 0" << endl;
+
+        else
+
+        {
+            answer = "";
+
+            fix(N);
+
+            cout << answer << endl;
+        }
+    }
+
+    return 0;
+}
