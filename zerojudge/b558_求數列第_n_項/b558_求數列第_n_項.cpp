7ab34b1e7fd9ca002d66d284ba7d055bff5da432
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-30 12:06:22 +0800

    feat(zerojudge): b558 求數列第 n 項

---
+++ zerojudge/b558_求數列第_n_項/b558_求數列第_n_項.cpp
@@ -0,0 +1,18 @@
+/*
+ Problem link: https://zerojudge.tw/ShowProblem?problemid=b558
+ Status: AC
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+
+    while (cin >> n) {
+        cout << ((n * (n - 1)) / 2 + 1) << endl;
+    }
+    return 0;
+}
