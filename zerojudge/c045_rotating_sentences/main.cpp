8f97510df04caac248140342c8fee023b067e8d4
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-07-31 11:57:20 +0800

    feat(zerojudge): c045 Rotating Sentences

---
+++ zerojudge/c045_rotating_sentences/main.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author: @1chooo
+ * Title: 490 - Rotating Sentences
+ * Required: frequency limit: 3.000 seconds
+ * Link: https://zerojudge.tw/ShowProblem?problemid=c045
+ * Status: AC
+ */
+
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string lines[101];
+    int setenceCount = 0;
+    int maxLineLength = 0;
+
+    while (getline(cin, lines[setenceCount])) {
+        if (maxLineLength < lines[setenceCount].length())
+            maxLineLength = lines[setenceCount].length();
+        setenceCount++;
+    }
+
+    for (int i = 0; i < maxLineLength; i++) {
+        for (int j = setenceCount - 1; j >= 0; j--) {
+            if (lines[j].length() > i)
+                cout << lines[j][i];
+            else
+                cout << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
