a85718e0749159b772e97e6ec602b09e1cf3bdd1
Shinnn <cindy93517@gmail.com>
2024-08-03 20:14:16 +0800

    feat(leetcode): 0001 Two Sum

--- leetcode/0001_two_sum/0001_two_sum.py
+++ leetcode/0001_two_sum/0001_two_sum.py
@@ -9,3 +9,19 @@ class Solution:
                 hMap[target - nums[i]] = i
 
         return []
+
+
+# author: Shin0517
+class Solution(object):
+    def twoSum(self, nums, target):
+        numDict = {}
+        n = len(nums)
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numDict:
+                return [numDict[complement], i]
+            else: 
+                numDict[nums[i]] = i
+
+        return []
