3da70374aff9b6a97a5ad24479af048fe5c7f53a
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-08-06 00:26:34 +0800

    feat(uva): 11541 Decoding

---
+++ uva/11541_decoding/11541_decoding.cpp
@@ -0,0 +1,24 @@
+// uva11541
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n, total, cnt = 0;
+    char alpha;
+
+    while (cin >> n) {
+        getchar();
+        for (int i = 1; i <= n; i++) {
+            cout << "Case " << i << ": ";
+            while ((alpha = getchar()) != '\n') {
+                cin >> total;
+                for (int i = 0; i < total; i++) {
+                    cout << alpha;
+                }
+            }
+            cout << endl;
+        }
+    }
+}
