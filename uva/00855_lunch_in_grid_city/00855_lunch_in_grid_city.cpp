7758b655247625301650f6c1e8aedfdaa133d951
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-08-07 13:40:43 +0800

    feat(uva): 00855 Launch in Grid City

--- uva/00855_lunch_in_grid_city/00855_lunch_in_grid_city.cpp
+++ uva/00855_lunch_in_grid_city/00855_lunch_in_grid_city.cpp
@@ -1,22 +1,25 @@
-//uva855
-#include <iostream>
+// uva855
 #include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
 int x[50005];
 int y[50005];
-int main(){
+int main() {
     int n;
-    cin>>n;
-    while(n--){
-        int S,A,F;
-        cin>>S>>A>>F;
-        for(int i=0;i<F;i++){
-            cin>>x[i]>>y[i];
+    cin >> n;
+    while (n--) {
+        int S, A, F;
+        cin >> S >> A >> F;
+        for (int i = 0; i < F; i++) {
+            cin >> x[i] >> y[i];
         }
-        sort(x,x+F);
-        sort(y,y+F);
-        if(F%2) cout<<"(Street: "<<x[F/2]<<", Avenue: "<<y[F/2]<<")"<<endl;
-        else cout<<"(Street: "<<x[(F-1)/2]<<", Avenue: "<<y[(F-1)/2]<<")"<<endl;
+        sort(x, x + F);
+        sort(y, y + F);
+        if (F % 2)
+            cout << "(Street: " << x[F / 2] << ", Avenue: " << y[F / 2] << ")" << endl;
+        else
+            cout << "(Street: " << x[(F - 1) / 2] << ", Avenue: " << y[(F - 1) / 2] << ")" << endl;
     }
 }
