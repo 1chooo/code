4af501169f0f4ef1d06749b58da4842c77a2a3b9
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-10-17 11:31:43 +0800

    feat(uva): 00846 Steps

--- uva/00846-steps/solution.cpp
+++ uva/00846-steps/solution.cpp
@@ -1,12 +1,21 @@
-// uva846
+/*
+ * Author: @1chooo
+ * Title: 846 - Steps
+ * Required: frequency limit: 3.000 seconds
+ * Link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=787
+ * Status: AC
+ */
+
 #include <cstdlib>
 #include <iostream>
+
 using namespace std;
 
 int main() {
-
     int m;
+
     cin >> m;
+    
     for (int i = 0; i < m; i++) {
         long long a, b;
         cin >> a >> b;
