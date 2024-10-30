87cabf97dcadbbb3fc9a37782df40a52029bec87
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-10-30 15:44:20 +0800

    feat(uva): 11498 Division of Nlogonia

--- uva/11498-division-of-nlogonia/solution.cpp
+++ uva/11498-division-of-nlogonia/solution.cpp
@@ -1,5 +1,13 @@
-// uva11498
-#include <bits/stdc++.h>
+/*
+ * Author: @1chooo
+ * Title: 11498 - Division of Nlogonia
+ * Required: frequency limit: 1.000 seconds
+ * Link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2493
+ * Status: AC
+ */
+
+#include <cstdio>
+#include <iostream>
 
 using namespace std;
 
