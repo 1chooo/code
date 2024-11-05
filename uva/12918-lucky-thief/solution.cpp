c7f826cef5d1d930df378df9a60503903ace0cd4
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-11-05 14:22:13 +0800

    feat(uva): 12918 Lucky Thief

--- uva/12918-lucky-thief/solution.cpp
+++ uva/12918-lucky-thief/solution.cpp
@@ -1,16 +1,26 @@
-// uva12918
+/*
+ * Author: @1chooo
+ * Title: 12918 - Lucky Thief
+ * Required: frequency limit: 1.000 seconds
+ * Link:
+ * Status: AC
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
+
 int main() {
     int n;
+    long long int a, b;
+    long long int ans;
+
     scanf("%d", &n);
+
     while (n--) {
-        long long int a, b;
         scanf("%lld %lld", &a, &b);
-        long long int ans;
         ans = (b + b - a - 1) * a / 2;
         printf("%lld\n", ans);
     }
+
     return 0;
 }
