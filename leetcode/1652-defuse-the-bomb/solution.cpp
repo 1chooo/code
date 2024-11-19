d793d80d478ba1fe3d1ae7ee71c4a3af69e5f039
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-11-19 15:47:11 +0800

    feat(leetcode): 1652 Defuse the Bomb

--- leetcode/1652-defuse-the-bomb/solution.cpp
+++ leetcode/1652-defuse-the-bomb/solution.cpp
@@ -20,6 +20,46 @@ class Solution {
             return code;
         }
 
+        for (int i = 0; i < n; i++) {
+            int total = 0;
+            if (k > 0) {
+                for (int j = 1; j <= k; j++) {
+                    total += code[(i + j) % n];
+                }
+            }
+            for (int j = 1; j <= -k; j++) {
+                total += code[(i - j + n) % n];
+            }
+
+            ans[i] = total;
+        }
+
+        return ans;
+    }
+};
+
+/**
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem: https://leetcode.com/problems/defuse-the-bomb
+ * Runtime: 0ms (100.00%)
+ */
+
+const static auto _ = []() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    return nullptr;
+}();
+
+class Solution {
+  public:
+    vector<int> decrypt(vector<int> &code, int k) {
+        int n = code.size();
+        vector<int> ans(n, 0);
+
+        if (k == 0) {
+            fill(code.begin(), code.end(), 0);
+            return code;
+        }
+
         if (k > 0) {
             for (int i = 0; i < n; i++) {
                 for (int j = 1; j <= k; j++) {
