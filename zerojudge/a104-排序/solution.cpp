56ed526d74c64d1955fc4f85d57b95a55058ad28
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-11-25 18:02:14 +0800

    feat(zerojudge): a104 排序

--- zerojudge/a104-排序/solution.cpp
+++ zerojudge/a104-排序/solution.cpp
@@ -1,30 +1,25 @@
 /*
- Problem link: https://zerojudge.tw/ShowProblem?problemid=a104
- Status: AC
-*/
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://zerojudge.tw/ShowProblem?problemid=a104
+ * Status: AC
+ */
 
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 int main(void) {
     int num;
-    
+
     while (cin >> num) {
         int arr[1000] = {};
         int temp;
-        
+
         for (int i = 0; i < num; i++) {
             cin >> temp;
             arr[i] = temp;
         }
-
-        // for (int i = 0; i < num; i++) {
-        //     cout << arr[i] << " ";
-        // }
-
-        // cout << "\n";
 
         for (int i = 0; i < num; i++) {
             int temp = i;
