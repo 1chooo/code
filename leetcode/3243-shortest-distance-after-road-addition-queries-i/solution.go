973e3c058964d808350f7eb81ccc54b42516962d
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-11-27 16:04:13 +0800

    feat(leetcode): 3243 Shortest Distance After Road Addition Queries I

---
+++ leetcode/3243-shortest-distance-after-road-addition-queries-i/solution.go
@@ -0,0 +1,5 @@
+/**
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem: https://leetcode.com/problems/shortest-distance-after-road-addition-queries-i/
+ * Runtime: ms (%)
+ */
---
+++ leetcode/3243-shortest-distance-after-road-addition-queries-i/solution.py
@@ -0,0 +1,6 @@
+"""
+* Author: 1chooo<hugo970217@gmail.com>
+* Problem: https://leetcode.com/problems/shortest-distance-after-road-addition-queries-i/
+* Runtime: ms (%)
+"""
+
