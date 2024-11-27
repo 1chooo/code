b15c4a224943de88b31d993c9fd7a52cf44c63a0
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-11-27 15:52:46 +0800

    feat(leetcode): 2924 Find Champion II

--- leetcode/2924-find-champion-ii/solution.cpp
+++ leetcode/2924-find-champion-ii/solution.cpp
@@ -12,6 +12,41 @@ const static auto _ = []() {
 class Solution {
   public:
     static int findChampion(int n, vector<vector<int>> &edges) {
+        vector<int> isGood(n, 1);
+
+        for (auto &edge : edges) {
+            isGood[edge[1]] = 0;
+        }
+
+        int champion = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (isGood[i]) {
+                if (champion != -1) {
+                    return -1;
+                }
+                champion = i;
+            }
+        }
+
+        return champion;
+    }
+};
+
+/**
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem: https://leetcode.com/problems/find-champion-ii/
+ * Runtime: 0ms (100.00%)
+ */
+
+const static auto _ = []() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    return nullptr;
+}();
+
+class Solution {
+  public:
+    static int findChampion(int n, vector<vector<int>> &edges) {
         vector<int> inDegree(n, 0);
 
         for (auto &edge : edges) {
--- leetcode/2924-find-champion-ii/solution.py
+++ leetcode/2924-find-champion-ii/solution.py
@@ -1,6 +1,22 @@
 """
 * Author: 1chooo<hugo970217@gmail.com>
 * Problem: https://leetcode.com/problems/find-champion-ii/
-* Runtime: ms (%)
+* Runtime: 0ms (100.00%)
 """
 
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        is_good: List[int] = [1] * n
+
+        for edge in edges:
+            is_good[edge[1]] = 0
+
+        champion: int = -1
+
+        for i in range(n):
+            if is_good[i] == 1:
+                if champion == -1:
+                    return -1
+                champion = i
+
+        return champion
