4ac73c17bdbd08832a981f306caa8b8c1a46ba4e
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-11-27 16:11:35 +0800

    feat(uva): 10036 Divisibility

---
+++ uva/10036-divisibility/solution.cpp
@@ -0,0 +1,35 @@
+/*
+ * Author: @1chooo
+ * Title: 10036 - Divisibility
+ * Required: Time limit: 3.000 seconds
+ * Link: 
+ * Status: AC
+ */
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int m, d;
+        cin >> m >> d;
+        int num[m];
+        for (int j = 0; j < m; j++) {
+            cin >> num[j];
+            num[j] = abs(num[j]) % d;
+        }
+        int f[m + 1][100] = {0};
+        f[0][0] = 1;
+        for (int j = 0; j < m; j++)
+            for (int k = 0; k < d; k++)
+                if (f[j][k]) {
+                    f[j + 1][(k + d + num[j]) % d] = 1;
+                    f[j + 1][(k + d - num[j]) % d] = 1;
+                }
+        cout << (f[m][0] ? "Divisible" : "Not divisible") << endl;
+    }
+    return 0;
+}
