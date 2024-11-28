211eaebaca125c6f30832e43373f3f423d11a7e0
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-11-28 18:16:10 +0800

    feat(zerojudge): a215 明明愛數數

--- zerojudge/a215-明明愛數數/solution.cpp
+++ zerojudge/a215-明明愛數數/solution.cpp
@@ -1,7 +1,8 @@
 /*
- Problem link: https://zerojudge.tw/ShowProblem?problemid=a215
- Status: AC
-*/
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://zerojudge.tw/ShowProblem?problemid=a215
+ * Status: AC
+ */
 
 #include <iostream>
 #include <cstdlib>
