519fcd030a8adbd0a55315e1e80001a2e6056ed3
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-01 01:10:25 +0800

    feat(uva): 10106 Product

---
+++ uva/10106-product/README.md
@@ -0,0 +1,27 @@
+# 10106 Product
+
+The problem is to multiply two integers \(X\) and \(Y\) where \(0 \leq X, Y < 10^{250}\).
+
+## Input
+
+The input will consist of a set of pairs of lines. Each line in a pair contains one multiplier.
+
+## Output
+
+For each input pair of lines, the output line should consist of one integer: the product.
+
+## Sample Input
+
+```
+12
+12
+2
+222222222222222222222222
+```
+
+## Sample Output
+
+```
+144
+444444444444444444444444
+```
---
+++ uva/10106-product/solution.cpp
@@ -0,0 +1,52 @@
+
+/*
+ * Author: @1chooo
+ * Title: 10106 - Product
+ * Required: Time limit: 3.000 seconds
+ * Link: https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=934
+ * Status: AC
+ */
+
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string solve(int n) {
+    string ans = "";
+    int i;
+
+    while (n != 1) {
+        for (i = 9; i > 1; i--) {
+            if (n % i == 0) {
+                n /= i;
+                ans = (char)(i + '0') + ans;
+                break;
+            }
+        }
+
+        if (i == 1)
+            return "-1";
+    }
+
+    return ans;
+}
+
+int main() {
+    int test_case, n;
+    string ans;
+
+    scanf("%d", &test_case);
+
+    while (test_case--) {
+        scanf("%d", &n);
+        if (n < 10) {
+            printf("%d\n", n);
+            continue;
+        }
+        cout << solve(n) << endl;
+    }
+
+    return 0;
+}
