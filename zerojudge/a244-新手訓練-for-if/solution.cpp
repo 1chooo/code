4f754c40c8fd389042bbe4bf8bc913faac44a87b
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-01 01:14:30 +0800

    feat(zerojudge): a244 新手訓練~for+if

--- zerojudge/a244-新手訓練-for-if/solution.cpp
+++ zerojudge/a244-新手訓練-for-if/solution.cpp
@@ -1,7 +1,8 @@
 /*
- Problem link: https://zerojudge.tw/ShowProblem?problemid=a224
- Status: AC
-*/
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://zerojudge.tw/ShowProblem?problemid=a244
+ * Status: AC
+ */
 
 #include <cmath>
 #include <cstdlib>
