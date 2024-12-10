c394a6450a96f8ae0a4f70cf0fec0d704bb403bd
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-10 18:10:25 +0800

    feat(zerojudge): a248 新手訓練~陣列應用

--- zerojudge/a248-新手訓練-陣列應用/solution.cpp
+++ zerojudge/a248-新手訓練-陣列應用/solution.cpp
@@ -1,7 +1,8 @@
 /*
- Problem link: https://zerojudge.tw/ShowProblem?problemid=a248
- Status: AC
-*/
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://zerojudge.tw/ShowProblem?problemid=a248
+ * Status: AC
+ */
 
 #include <cstdlib>
 #include <iostream>
@@ -10,12 +11,12 @@ using namespace std;
 
 int main(void) {
     long long int a = 0, b = 0;
-    int N;
+    int caseCount;
 
-    while (cin >> a >> b >> N) {
+    while (cin >> a >> b >> caseCount) {
         cout << a / b << ".";
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= caseCount; i++) {
             cout << ((a % b) * 10) / b;
             a = ((a % b) * 10) % b;
         }
