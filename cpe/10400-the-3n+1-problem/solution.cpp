2d15d45ab0339336a9d96bde64489d6151f39e89
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-12-26 10:06:20 +0800

    feat(cpe): 10400 The 3n + 1 Problem

--- cpe/10400-the-3n+1-problem/solution.cpp
+++ cpe/10400-the-3n+1-problem/solution.cpp
@@ -6,51 +6,51 @@
  * Status: AC
  */
 
-#include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 int main() {
 
-  long long int i, j, temp, n;
-  long long int operation, maxOperation;
+    long long int i, j, temp, n;
+    long long int operation, maxOperation;
 
-  while (cin >> i >> j) {
-    bool change = false;
+    while (cin >> i >> j) {
+        bool change = false;
 
-    if (i > j) {
-      temp = i;
-      i = j;
-      j = temp;
-      change = true;
-    }
+        if (i > j) {
+            temp = i;
+            i = j;
+            j = temp;
+            change = true;
+        }
 
-    maxOperation = 0;
+        maxOperation = 0;
 
-    for (int a = i; a <= j; a++) {
-      n = a;
-      operation = 1;
+        for (int a = i; a <= j; a++) {
+            n = a;
+            operation = 1;
 
-      while (n != 1) {
+            while (n != 1) {
 
-        if ((n & 1) == 0)
-          (n >>= 1);
+                if ((n & 1) == 0)
+                    (n >>= 1);
+                else
+                    n = 3 * n + 1;
+                operation++;
+            }
+
+            if (operation > maxOperation)
+                maxOperation = operation;
+        }
+
+        if (change == true)
+            printf("%lld %lld %lld\n", j, i, maxOperation);
         else
-          n = 3 * n + 1;
-        operation++;
-      }
-
-      if (operation > maxOperation)
-        maxOperation = operation;
+            printf("%lld %lld %lld\n", i, j, maxOperation);
     }
 
-    if (change == true) 
-      printf("%lld %lld %lld\n", j, i, maxOperation);
-    else 
-      printf("%lld %lld %lld\n", i, j, maxOperation);
-  }
-  
-  return 0;
+    return 0;
 }
