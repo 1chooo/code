2511339c9597f0eb6c602caac8a125edffb05445
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-15 16:31:59 +0800

    chore(cpe): 00108 Maximum Sum
    Array 'sum[101][101]' accessed at index sum[*][-1], which is out of bounds.

--- cpe/00108-maximum-sum/solution.cpp
+++ cpe/00108-maximum-sum/solution.cpp
@@ -30,24 +30,18 @@ void print_arr(const int arr[SIZE][SIZE], int n) {
 }
 
 int solve(int n) {
-    // set sum(input[1][1] ~ input[i][j]) to sum[i][j]
     for (int i = 0; i <= n; i++)
         for (int j = 0; j <= n; j++)
-            sum[i][j] = sum[i - 1][j] +
-                        sum[i][j - 1] +
+            sum[i][j] = (i > 0 ? sum[i - 1][j] : 0) +
+                        (j > 0 ? sum[i][j - 1] : 0) +
                         input[i][j] -
-                        sum[i - 1][j - 1];
+                        (i > 0 && j > 0 ? sum[i - 1][j - 1] : 0);
 
-    // for debug
-    // print_arr(sum, N);
-
-    // get maximum sum of sum(input[i][j] ~ input[x][y])
     int maximum = INT_MIN;
     for (int x = 0; x <= n; x++) {
         for (int y = 0; y <= n; y++) {
             for (int i = 0; i <= x; i++) {
                 for (int j = 0; j <= y; j++) {
-                    // the s is sum of [i][j] ~ [x][y])
                     int s = sum[x][y] -
                             sum[i][y] -
                             sum[x][j] +
