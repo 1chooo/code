977476f9bd198544e1362c324e3dc2c40d4cb395
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-20 22:35:03 +0800

    feat(zerojudge): e926 跳脫字元

--- zerojudge/e926-跳脫字元/solution.cpp
+++ zerojudge/e926-跳脫字元/solution.cpp
@@ -1,7 +1,8 @@
 /*
- Problem link: https://zerojudge.tw/ShowProblem?problemid=e926
- Status: AC
-*/
+ * Author: @1chooo<hugo970217@gmail.com>
+ * Problem link: https://zerojudge.tw/ShowProblem?problemid=e926
+ * Status: AC
+ */
 
 #include <cstdlib>
 #include <iostream>
