11183878c4d14df834a6a24e3c79a6c508404407
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-25 00:32:08 +0800

    feat(uva): 10252 Common Permutation

--- uva/10252-common-permutation/solution.cpp
+++ uva/10252-common-permutation/solution.cpp
@@ -1,41 +1,38 @@
 /*
- * Author: @1chooo
- * Title: 10252 - Common Permutation
- * Required: frequency limit: 3.000 seconds
- * Link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=1193
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=1193
  * Status: AC
  */
 
-#include <iostream>
-#include <cstdlib>
-#include <string>
 #include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(void) {
+    string first;
+    string second;
 
-  string first, second;
-  int countFirst, countSecond;
+    while (getline(cin, first) && getline(cin, second)) {
+        int countFirst = 0;
+        int countSecond = 0;
+        int letter[26] = {0};
 
-  while (getline(cin, first) && getline(cin, second)) {
-    countFirst = 0;
-    countSecond = 0;
+        for (int i = 0; i < 26; i++) {
+            countFirst = count(first.begin(), first.end(), 'a' + i);
+            countSecond = count(second.begin(), second.end(), 'a' + i);
 
-    int letter[26] = {0};
+            letter[i] = min(countFirst, countSecond);
 
-    for (int i = 0; i < 26; i++) {
-      countFirst = count(first.begin(), first.end(), 'a' + i);
-      countSecond = count(second.begin(), second.end(), 'a' + i);
+            if (letter[i] != 0)
+                for (int j = 0; j < letter[i]; j++)
+                    cout << static_cast<char>('a' + i);
+        }
 
-      letter[i] = min(countFirst, countSecond);
-
-      if (letter[i] != 0)
-        for (int j = 0; j < letter[i]; j++)
-          cout << static_cast<char>('a' + i);
+        cout << endl;
     }
-    cout << endl;
-  }
 
-  return 0;
+    return 0;
 }
