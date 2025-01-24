758b4960430c15e1b468f9b40d5fc01a34f3807b
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-25 00:35:10 +0800

    feat(uva): 00100 The 3n + 1 problem

--- uva/00100-the-3n+1-problem/solution.cpp
+++ uva/00100-the-3n+1-problem/solution.cpp
@@ -1,55 +1,52 @@
 /*
- * Author: @1chooo
- * Title: 100 - The 3n + 1 problem
- * Required: Time limit: 3.000 seconds
- * Link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=36
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=36
  * Status: AC
  */
 
-#include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 int main() {
+    long long int i;
+    long long int j;
 
-  long long int i, j, temp, n;
-  long long int operation, maxOperation;
+    while (cin >> i >> j) {
+        bool change = false;
 
-  while (cin >> i >> j) {
-    bool change = false;
+        if (i > j) {
+            long long int temp = i;
+            i = j;
+            j = temp;
+            change = true;
+        }
 
-    if (i > j) {
-      temp = i;
-      i = j;
-      j = temp;
-      change = true;
-    }
+        int maxOperation = 0;
 
-    maxOperation = 0;
+        for (int a = i; a <= j; a++) {
+            long long int n = a;
+            int operation = 1;
 
-    for (int a = i; a <= j; a++) {
-      n = a;
-      operation = 1;
+            while (n != 1) {
+                if (n % 2 == 1)
+                    n = 3 * n + 1;
+                else
+                    n /= 2;
+                operation++;
+            }
 
-      while (n != 1) {
-        if (n % 2 == 1)
-          n = 3 * n + 1;
+            if (operation > maxOperation)
+                maxOperation = operation;
+        }
+
+        if (change == true)
+            printf("%lld %lld %lld\n", j, i, maxOperation);
         else
-          n /= 2;
-        operation++;
-      }
-
-      if (operation > maxOperation)
-        maxOperation = operation;
+            printf("%lld %lld %lld\n", i, j, maxOperation);
     }
 
-    if (change == true) 
-      printf("%lld %lld %lld\n", j, i, maxOperation);
-    else 
-      printf("%lld %lld %lld\n", i, j, maxOperation);
-  }
-  
-  return 0;
+    return 0;
 }
