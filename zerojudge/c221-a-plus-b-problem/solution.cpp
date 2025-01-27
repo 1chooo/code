80b54ea9e19c6439ca7fa75382b327d00cfff56b
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-27 15:02:01 +0800

    feat(zerojudge): c221 A+B problem (駭客題)

---
+++ zerojudge/c221-a-plus-b-problem/solution.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://zerojudge.tw/ShowProblem?problemid=c221
+ * Status: AC
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << "1000000000000" << " " << "1" << endl;
+
+    return 0;
+}
