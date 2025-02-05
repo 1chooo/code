4585606f630b5ee4a95889890765d65d8d45aefd
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-02-05 14:07:52 +0800

    chore(uva): 10242 Fourth Point

--- uva/10242-fourth-point/solution.cpp
+++ uva/10242-fourth-point/solution.cpp
@@ -1,16 +1,33 @@
+/*
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1183
+ * Status: AC
+ */
+
 #include <cstdio>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    double pointX[4], pointY[4];
-    int index[2]; // the index of overlapping point
-    double firstX, firstY, secondX, secondY, thirdX, thirdY;
-    double fourthX, fourthY;
-    double vector1_X, vector1_Y, vector2_X, vector2_Y;
+    double pointX[4];
+    double pointY[4];
 
     while (cin >> pointX[0] >> pointY[0]) {
+        int index[2]; // the index of overlapping point
+        double firstX;
+        double firstY;
+        double secondX;
+        double secondY;
+        double thirdX;
+        double thirdY;
+        double fourthX;
+        double fourthY;
+        double vector1_X;
+        double vector1_Y;
+        double vector2_X;
+        double vector2_Y;
+
         for (int i = 1; i < 4; i++) {
             cin >> pointX[i] >> pointY[i];
         }
@@ -18,7 +35,8 @@ int main() {
         // find the overlappig point
         for (int i = 0; i < 4; i++) {
             for (int j = i + 1; j < 4; j++) {
-                if (pointX[i] == pointX[j] && pointY[i] == pointY[j]) {
+                if (pointX[i] == pointX[j] &&
+                    pointY[i] == pointY[j]) {
                     index[0] = i;
                     index[1] = j;
                 }
@@ -54,5 +72,6 @@ int main() {
 
         printf("%.3f %.3f\n", fourthX, fourthY);
     }
+
     return 0;
 }
--- uva/10242-fourth-point/solution.py
+++ uva/10242-fourth-point/solution.py
@@ -1,21 +1,39 @@
-def find_fourth_point(x1, y1, x2, y2, x3, y3, x4, y4):
-    if x1 == x3 and y1 == y3: # 檢查點1和點3是否相同（即它們是重複的點）
+"""
+Author: 1chooo<hugo970217@gmail.com>
+Problem link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1183
+Status: AC
+"""
+
+
+def find_fourth_point(
+    x1: float,
+    y1: float,
+    x2: float,
+    y2: float,
+    x3: float,
+    y3: float,
+    x4: float,
+    y4: float,
+) -> str:
+    if x1 == x3 and y1 == y3:
         return f"{(x2 + x4) - x1:.3f} {(y2 + y4) - y1:.3f}"
-    elif x1 == x4 and y1 == y4: # 檢查點1和點4是否相同
+    elif x1 == x4 and y1 == y4:
         return f"{(x2 + x3) - x1:.3f} {(y2 + y3) - y1:.3f}"
-    elif x2 == x3 and y2 == y3: # 檢查點2和點3是否相同
+    elif x2 == x3 and y2 == y3:
         return f"{(x1 + x4) - x2:.3f} {(y1 + y4) - y2:.3f}"
-    else: # 如果上述條件都不符合，則點2和點4是中點
+    else:
         return f"{(x1 + x3) - x2:.3f} {(y1 + y3) - y2:.3f}"
 
 
-def main():
+def main() -> None:
     while True:
         try:
             x1, y1, x2, y2, x3, y3, x4, y4 = map(float, input().split())
-            print(find_fourth_point(x1, y1, x2, y2, x3, y3, x4, y4))
+            result = find_fourth_point(x1, y1, x2, y2, x3, y3, x4, y4)
+            print(result)
         except EOFError:
             break
+
 
 if __name__ == "__main__":
     main()
