0897d640c5f8600a5415535f3b35e9a865654a78
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-02-06 21:17:50 +0800

    feat(zerojudge): c316 最遠點對！前傳

--- zerojudge/c316-最遠點對-前傳/solution.cpp
+++ zerojudge/c316-最遠點對-前傳/solution.cpp
@@ -1,7 +1,8 @@
 /*
- Problem link: https://zerojudge.tw/ShowProblem?problemid=c316
- Status: AC
-*/
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://zerojudge.tw/ShowProblem?problemid=c316
+ * Status: AC
+ */
 
 #include <cmath>
 #include <cstdlib>
@@ -10,35 +11,36 @@
 using namespace std;
 
 int main(void) {
-    int N = 0;
+    int num = 0;
 
-    cin >> N;
-    int x[N];
-    int y[N];
+    cin >> num;
+    int x[num];
+    int y[num];
 
-    for (int i = 0; i < N; i++) {
-        int tempX = 0, tempY = 0;
+    for (int i = 0; i < num; i++) {
+        int tempX = 0;
+        int tempY = 0;
 
         cin >> tempX >> tempY;
         x[i] = tempX;
         y[i] = tempY;
     }
 
-    long long int maxDistance = 0, distance = 0;
+    long long int maxDistance = 0;
+    long long int distance = 0;
     int minimumPoint = 0;
     int maxPoint = 0;
 
-    for (int i = 0; i < N - 1; i++) {
-        for (int j = i + 1; j < N; j++) {
-            distance = abs(((x[i] - x[j]) * (x[i] - x[j])) + ((y[i] - y[j]) * (y[i] - y[j])));
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = i + 1; j < num; j++) {
+            distance = abs(((x[i] - x[j]) * (x[i] - x[j])) +
+                           ((y[i] - y[j]) * (y[i] - y[j])));
 
             if (distance > maxDistance) {
                 maxDistance = distance;
                 minimumPoint = i;
                 maxPoint = j;
             }
-
-            // cout << maxDistance << ", " << minimumPoint << ", " << maxPoint << endl;
         }
     }
 
