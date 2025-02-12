d9bb531538e569c9f32da08827059e0e7fe13559
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-02-12 19:34:57 +0800

    feat(zerojudge): d483 hello, world

---
+++ zerojudge/d483-hello-world/solution.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://zerojudge.tw/ShowProblem?problemid=d483
+ * Status: AC
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << "hello, world" << endl;
+
+    return 0;
+}
