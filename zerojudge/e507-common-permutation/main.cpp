53f062843116e0903223c808761882d7e2d48a25
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-02-28 01:07:17 +0800

    feat(zerojudge): e507 Common Permutation

---
+++ zerojudge/e507-common-permutation/main.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://zerojudge.tw/ShowProblem?problemid=e507
+ * Status: AC
+ */
+
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string first;
+    string second;
+
+    while (getline(cin, first) && getline(cin, second)) {
+        int countFirst = 0;
+        int countSecond = 0;
+        int letter[26] = {0};
+
+        for (int i = 0; i < 26; i++) {
+            countFirst = count(first.begin(), first.end(), 'a' + i);
+            countSecond = count(second.begin(), second.end(), 'a' + i);
+
+            letter[i] = min(countFirst, countSecond);
+
+            if (letter[i] != 0)
+                for (int j = 0; j < letter[i]; j++)
+                    cout << static_cast<char>('a' + i);
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
