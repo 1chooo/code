8f811c5cf8dd40619f00c8a3fa9d3aff180934e4
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-04-04 18:29:31 +0800

    chore(leetcode): 0001 Two Sum

--- leetcode/0001-two-sum/solution.py
+++ leetcode/0001-two-sum/solution.py
@@ -1,18 +1,17 @@
 """
 * Author: 1chooo<hugo970217@gmail.com>
 * Problem: https://leetcode.com/problems/two-sum/
-* Runtime: 3ms (89.08%)
+* Runtime: 0ms (100.00%)
 """
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hMap = {}
+        hash_map = {}
         
         for i in range(len(nums)):
-            if nums[i] in hMap:
-                return [hMap[nums[i]], i]
-            else:
-                hMap[target - nums[i]] = i
+            if nums[i] in hash_map:
+                return [hash_map[nums[i]], i]
+            hash_map[target - nums[i]] = i
 
         return []
 
