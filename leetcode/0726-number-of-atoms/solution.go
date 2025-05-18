22b3176b693ea93f0dc9386fd983b67527e1a439
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-05-19 02:22:28 +0800

    feat(leetcode): 0726 Number of Atoms

--- leetcode/0726-number-of-atoms/solution.go
+++ leetcode/0726-number-of-atoms/solution.go
@@ -1,3 +1,9 @@
+/**
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem: https://leetcode.com/problems/number-of-atoms/
+ * Runtime: 0ms (100.00%)
+ */
+
 func countOfAtoms(formula string) string {
 	mul := 1
 	muls := []int{}
@@ -50,52 +56,4 @@ func countOfAtoms(formula string) string {
 	}
 
 	return res.String()
-}
-
-// WA
-func countOfAtoms(formula string) string {
-    mul := 1
-	muls := []int{}
-	digits := 0
-	base := 1
-	atom := ""
-	atoms := map[string]int{}
-	for i := len(formula) - 1; i >= 0; i-- {
-		if formula[i] >= '0' && formula[i] <= '9' {
-			digits += base * int(formula[i] - '0')
-			base *= 10
-		} else if formula[i] == ')' {
-			if digits == 0 {
-				digits = 1
-			}
-			muls = append(muls, digits)
-			mul *= digits
-			digits = 0
-			base = 1
-		} else if formula[i] == '(' {
-			mul /= muls[len(muls)-1]
-			muls = muls[:len(muls)-1]
-		} else if formula[i] >= 'a' && formula[i] <= 'z' {
-			atom = string(formula[i]) + atom
-		} else {
-			atom = string(formula[i]) + atom
-			if digits == 0 {
-				digits = 1
-			}
-			atoms[atom] += mul * digits
-			digits = 0
-			base = 1
-			atom = ""
-		}
-	}
-
-	res := ""
-	for k, v := range atoms {
-		res += k
-		if v != 1 {
-			res += strconv.Itoa(v)
-		}
-	}
-
-	return res
 }
