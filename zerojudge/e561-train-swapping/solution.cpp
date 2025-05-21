423200f86d05ea97e0b8c80b81972766b5c6e818
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-05-21 11:00:39 +0800

    feat(zerojudge): e561 Train Swapping

---
+++ zerojudge/e561-train-swapping/solution.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://zerojudge.tw/ShowProblem?problemid=e561
+ * Status: AC
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+
+    scanf("%d", &n);
+
+    while (n--) {
+        int tr[100];
+
+        for (int i = 0; i < 100; i++)
+            tr[i] = 0;
+
+        int count = 0;
+        int l;
+
+        scanf("%d", &l);
+
+        for (int i = 0; i < l; i++)
+            scanf("%d", &tr[i]);
+
+        for (int i = 0; i < l - 1; i++) {
+            for (int k = 0; k < l - 1; k++) {
+                if (tr[k] > tr[k + 1]) {
+                    int t = tr[k];
+                    tr[k] = tr[k + 1];
+                    tr[k + 1] = t;
+                    count++;
+                }
+            }
+        }
+
+        printf("Optimal train swapping takes %d swaps.\n", count);
+    }
+
+    return 0;
+}
