0a245df4449cb0b9e53f76e8a06c4fcfc203aa97
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-06-01 12:01:47 +0800

    refactor(uva): 10420 List of Conquests

--- uva/10420-list-of-conquests/solution.c
+++ uva/10420-list-of-conquests/solution.c
@@ -1,8 +1,6 @@
 /*
- * Author: @1chooo
- * Title: 10420 - List of Conquests
- * Required: Time limit: 3.000 seconds
- * Link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=16&page=show_problem&problem=1361
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=16&page=show_problem&problem=1361
  * Status: AC
  */
 
@@ -18,21 +16,20 @@ typedef struct {
 } Country;
 
 int main(void) {
-    int lines = 0, i = 0;
-    char country[MAX_LEN];
-    char tmp[MAX_LEN];
+    int lines = 0;
 
     Country countries[MAX_LEN];
 
     scanf("%d", &lines);
     getchar();
 
-    for (i = 0; i < lines; i++) {
-        scanf("%s", country);
+    for (int i = 0; i < lines; i++) {
+        char country[MAX_LEN];
+        scanf("%99s", country);
         getchar();
 
-        int j, found = 0;
-        for (j = 0; j < i; j++) {
+        int found = 0;
+        for (int j = 0; j < i; j++) {
             if (strcmp(countries[j].name, country) == 0) {
                 countries[j].count++;
                 found = 1;
@@ -46,74 +43,14 @@ int main(void) {
             countries[i].count = 1;
         }
 
+        char tmp[MAX_LEN];
+
         fgets(tmp, MAX_LEN, stdin);
     }
 
-    for (i = 0; i < lines; i++) {
+    for (int i = 0; i < lines; i++) {
         printf("%s %d\n", countries[i].name, countries[i].count);
         free(countries[i].name);
-    }
-
-    return 0;
-}
-
-/*
- * Author: @1chooo
- * Title: 10420 - List of Conquests
- * Required: Time limit: 3.000 seconds
- * Link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=16&page=show_problem&problem=1361
- * Status: AC
- */
-
-#include <stdbool.h>
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-
-    int n;
-    int i, j;
-    int count, flag;
-    char country[2001][76] = {0};
-    char name[76] = {0};
-    char temp[76] = {0};
-
-    scanf("%d", &n);
-
-    for (i = 0; i < n; i++) {
-        scanf("%s", country[i]);
-        gets(name);
-    }
-
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - 1 - i; j++) {
-            if (strcmp(country[j], country[j + 1]) > 0) {
-                strcpy(temp, country[j]);
-                strcpy(country[j], country[j + 1]);
-                strcpy(country[j + 1], temp);
-            }
-        }
-    }
-
-    flag = false;
-    count = 0;
-    i = 0;
-
-    while (i < n) {
-        if (!flag) {
-            printf("%s ", country[i]);
-            count++;
-            flag = true;
-        } else if (flag) {
-            if (strcmp(country[i], country[i + 1]) == 0) {
-                count++;
-            } else {
-                printf("%d\n", count);
-                flag = false;
-                count = 0;
-            }
-            i++;
-        }
     }
 
     return 0;
--- uva/10420-list-of-conquests/solution.cpp
+++ uva/10420-list-of-conquests/solution.cpp
@@ -1,8 +1,6 @@
 /*
- * Author: @1chooo
- * Title: 10420 - List of Conquests
- * Required: Time limit: 3.000 seconds
- * Link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=16&page=show_problem&problem=1361
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=16&page=show_problem&problem=1361
  * Status: AC
  */
 
@@ -14,204 +12,21 @@
 using namespace std;
 
 int main(void) {
-
     int lines = 0;
-    int i = 0;
     map<string, int> countries;
-    string tmp;
 
     cin >> lines;
 
     while (lines--) {
+        string tmp;
         cin >> tmp;
+        
         countries[tmp]++;
         getline(cin, tmp);
     }
 
-    for (map<string, int>::iterator i = countries.begin(); i != countries.end(); i++)
+    for (map<string, int>::iterator i = countries.begin(); i != countries.end(); ++i)
         cout << i->first << " " << i->second << endl;
-    return 0;
-}
 
-/*
- * Author: @1chooo
- * Title: 10420 - List of Conquests
- * Required: Time limit: 3.000 seconds
- * Link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=16&page=show_problem&problem=1361
- * Status: AC
- */
-
-#include <cstdio>
-#include <iostream>
-#include <map>
-#include <string.h>
-
-using namespace std;
-
-int main() {
-    int n;
-    map<string, int> record;
-    while (scanf("%d", &n) == 1) {
-        string country, tmp;
-        while (n--) {
-            cin >> country;
-            record[country]++;
-            getline(cin, tmp);
-        }
-        for (map<string, int>::iterator i = record.begin(); i != record.end(); i++)
-            cout << i->first << " " << i->second << endl;
-    }
-    return 0;
-}
-
-/*
- * Author: @1chooo
- * Title: 10420 - List of Conquests
- * Required: Time limit: 3.000 seconds
- * Link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=16&page=show_problem&problem=1361
- * Status: AC
- */
-
-#include <cstdlib>
-#include <iostream>
-#include <map>
-#include <string>
-
-using namespace std;
-
-int main() {
-
-    int n;
-    string str;
-    map<string, int> mp;
-
-    cin >> n;
-
-    while (n--) {
-        cin >> str;
-        mp[str]++;
-        getline(cin, str);
-    }
-
-    for (auto i : mp) {
-        cout << i.first << " " << i.second << endl;
-    }
-
-    return 0;
-}
-
-/*
- * Author: @1chooo
- * Title: 10420 - List of Conquests
- * Required: Time limit: 3.000 seconds
- * Link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=16&page=show_problem&problem=1361
- * Status: AC
- */
-
-#include <algorithm>
-#include <iostream>
-#include <string>
-using namespace std;
-int compare(const void *, const void *);
-
-int main() {
-    int n, count, j;
-    string name;
-
-    cin >> n;
-    string countries[n];
-
-    for (int i = 0; i < n; i++) {
-        cin >> countries[i];
-        getline(cin, name);
-    }
-
-    sort(countries, countries + n);
-
-    for (int i = 0; i < n;) {
-        cout << countries[i] << " ";
-        count = 1;
-
-        for (j = i + 1; j < n; j++) {
-            // countries[] has been sorted,
-            // so if c[i] != c[j] present that it won't have the same country after c[j]
-            if (countries[i] != countries[j]) {
-                break;
-            }
-            count++;
-        }
-        i = j;
-        cout << count << endl;
-    }
-    return 0;
-}
-
-/*
- * Author: @1chooo
- * Title: 10420 - List of Conquests
- * Required: Time limit: 3.000 seconds
- * Link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=16&page=show_problem&problem=1361
- * Status: AC
- */
-
-#include <algorithm>
-#include <cstdlib>
-#include <iostream>
-#include <map>
-#include <string>
-
-using namespace std;
-
-int main() {
-    map<string, int> c;
-    int n;
-    cin >> n;
-    string str;
-    getline(cin, str);
-    for (int i = 0; i < n; i++) {
-        string tem = "";
-        getline(cin, str);
-        for (auto iter = str.begin(); iter != str.end(); iter++) {
-            if (*iter == ' ')
-                break;
-            tem += *iter;
-        }
-        c[tem]++;
-    }
-    for (auto i = c.begin(); i != c.end(); i++)
-        cout << i->first << " " << i->second << "\n";
-    return 0;
-}
-
-/*
- * Author: @1chooo
- * Title: 10420 - List of Conquests
- * Required: Time limit: 3.000 seconds
- * Link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&category=16&page=show_problem&problem=1361
- * Status: AC
- */
-
-#include <iostream>
-#include <map>
-#include <string>
-
-using namespace std;
-int main() {
-    map<string, int> count;
-    map<string, int>::iterator iter;
-    string first_s;
-    char others[76] = {0};
-    int n;
-    cin >> n;
-    cin.ignore();
-    while (n--) {
-        cin >> first_s;
-        count[first_s]++;
-        cin.getline(others, 76);
-    }
-    for (iter = count.begin(); iter != count.end(); iter++) {
-        cout << iter->first << " ";
-        cout << iter->second << endl;
-    }
     return 0;
 }
