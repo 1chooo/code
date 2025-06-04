99a31a3af8d8c4e38604234fe2063aae61ca8128
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-06-04 22:38:08 +0800

    refactor(uva): 01237 Expert Enough

--- uva/01237-expert-enough/solution.cpp
+++ uva/01237-expert-enough/solution.cpp
@@ -1,8 +1,6 @@
 /*
- * Author: @1chooo<hugo970217@gmail.com>
- * Title: 1237 - Expert Enough?
- * Required: frequency limit: 3.000 seconds
- * Link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3678
+ * Author: 1chooo<hugo970217@gmail.com>
+ * Problem link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3678
  * Status: AC
  */
 
@@ -12,29 +10,42 @@
 int main(void) {
     int n;
     scanf("%d", &n);
+
     while (n--) {
-        int k, m, p, i;
-        int data1[10000], data2[10000];
-        char name[10000][25];
+        int k;
         scanf("%d", &k);
-        for (i = 0; i < k; i++)
-            scanf("%s %d %d", name[i], &data1[i], &data2[i]);
+
+        int data1[10000];
+        int data2[10000];
+        char name[10000][25];
+        for (int i = 0; i < k; i++)
+            scanf("%24s %d %d", name[i], &data1[i], &data2[i]);
+
+        int m;
         scanf("%d", &m);
         while (m--) {
+            int p;
             scanf("%d", &p);
-            int temp = 0, key;
-            for (i = 0; i < k; i++) {
+
+            int temp = 0;
+            int key;
+
+            for (int i = 0; i < k; i++) {
                 if (data1[i] <= p && p <= data2[i])
                     temp++, key = i;
-                if (temp > 1) break;
+                if (temp > 1)
+                    break;
             }
+
             if (temp == 1)
                 printf("%s\n", name[key]);
             else
                 printf("UNDETERMINED\n");
         }
+
         if (n)
             printf("\n");
     }
+
     return 0;
 }
