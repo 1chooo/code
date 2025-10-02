9c0d93fff81d99f43f4fa7aaa0c3518705ff4dec
Lin Hugo <hugo970217@gmail.com>
2025-10-02 12:57:45 -0700

    feat(leetcode-grind-75): 0001 Two Sum

--- leetcode-grind-75/0001-two-sum/solution.py
+++ leetcode-grind-75/0001-two-sum/solution.py
@@ -7,10 +7,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash_map = {}
-        
+
         for i in range(len(nums)):
-            if nums[i] in hash_map:
-                return [hash_map[nums[i]], i]
-            hash_map[target - nums[i]] = i
+            needed = target - nums[i]
+            if needed in hash_map:
+                return [hash_map[needed], i]
+            hash_map[nums[i]] = i
 
         return []
