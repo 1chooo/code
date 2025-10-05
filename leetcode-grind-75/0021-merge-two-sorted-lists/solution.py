8dfec5c2bf97e242bc7e60375c7f4722f4a42409
Lin Hugo <hugo970217@gmail.com>
2025-10-04 17:34:47 -0700

    feat(leetcode-grind-75): 0021 Merge Two Sorted Lists

---
+++ leetcode-grind-75/0021-merge-two-sorted-lists/solution.py
@@ -0,0 +1,58 @@
+"""
+* Author: 1chooo<hugo@1chooo.com>
+* Problem: https://leetcode.com/problems/merge-two-sorted-lists/
+* Runtime: 0ms (100.00%)
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        dummy = ListNode()
+        curr = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+
+        return dummy.next
+
+
+class Solution2:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
