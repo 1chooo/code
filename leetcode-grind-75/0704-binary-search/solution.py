69aad8525694fe9b0a1c6cea9d7e4037edd92373
Lin Hugo <hugo970217@gmail.com>
2025-10-13 16:43:29 -0700

    feat(leetcode-grind-75): 0742 Binary Search

---
+++ leetcode-grind-75/0704-binary-search/solution.py
@@ -0,0 +1,22 @@
+"""
+* Author: 1chooo<hugo@1chooo.com>
+* Problem: https://leetcode.com/problems/binary-search
+* Runtime: 0ms (100.00%)
+"""
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
