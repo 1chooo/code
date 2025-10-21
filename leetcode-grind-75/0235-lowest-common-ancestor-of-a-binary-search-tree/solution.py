c5bf808af996209e8ce957ec418d8dc11031d472
Lin Hugo <hugo970217@gmail.com>
2025-10-21 15:35:03 -0700

    feat(leetcode-grind-75): 0235 Lowest Common Ancestor of a Binary Search Tree

---
+++ leetcode-grind-75/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
@@ -0,0 +1,25 @@
+"""
+* Author: 1chooo<hugo@1chooo.com>
+* Problem: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree
+* Runtime: 56ms (71.16%)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        while root:
+            if p.val > root.val and q.val > root.val:
+                root = root.right
+            elif p.val < root.val and q.val < root.val:
+                root = root.left
+            else:
+                return root
