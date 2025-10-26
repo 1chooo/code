4c29a0c962a3dedf5f2d5609abcfbb4f30fa2b9a
Lin Hugo <hugo970217@gmail.com>
2025-10-26 16:30:54 -0700

    feat(leetcode-grind-75): 0104 Maximum Depth of Binary Tree

---
+++ leetcode-grind-75/0104-maximum-depth-of-binary-tree/solution.py
@@ -0,0 +1,22 @@
+"""
+* Author: Lin Hugo <hugo@1chooo.com>
+* Problem: https://leetcode.com/problems/maximum-depth-of-binary-tree
+* Runtime: 0ms (100.00%)
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return 1 + max(left, right)
