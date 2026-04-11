b7ecd67fe8d6b483801f517aa141cf76189b0d79
Lin Hugo <hugo970217@gmail.com>
2026-04-11 00:34:58 -0700

    feat(leetcode-grind-75): 0236 Lowest Common Ancestor of a Binary Tree

---
+++ leetcode-grind-75/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
@@ -0,0 +1,32 @@
+"""
+* Author: Lin Hugo<hugo@1chooo.com>
+* Problem: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree
+* Runtime: 61ms (52.44%)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        return left if left else right
