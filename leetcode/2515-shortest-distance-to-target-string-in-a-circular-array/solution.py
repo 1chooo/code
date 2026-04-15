81978ee178f25f65970cae680d2757068edf7753
Lin Hugo <hugo970217@gmail.com>
2026-04-15 16:51:21 -0700

    feat(leetcode): 2515 Shortest Distance to Target String in a Circular Array

--- leetcode/2515-shortest-distance-to-target-string-in-a-circular-array/solution.py
+++ leetcode/2515-shortest-distance-to-target-string-in-a-circular-array/solution.py
@@ -1,6 +1,21 @@
 """
 * Author: 1chooo<hugo970217@gmail.com>
 * Problem: https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array
-* Runtime: ms (%)
+* Runtime: 0ms (100.00%)
 """
 
+class Solution:
+    def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
+        n = len(words)
+        best = float("inf")
+
+        for i in range(n):
+            if words[i] == target:
+                diff = abs(i - startIndex)
+                dist = min(diff, n - diff)
+                best = min(best, dist)
+
+        if best == float("inf"):
+            return -1
+
+        return best
